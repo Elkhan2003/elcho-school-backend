40afc2e40720bf9144ae02f6a3f40ec9b7b5c3c2
Elcho911 <boss.armsport@gmail.com>
2023-09-14 01:57:34 +0600

    added google auth

--- dist/app.js
+++ dist/app.js
@@ -6,8 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.buildServer = void 0;
 const dotenv_1 = require("dotenv");
 (0, dotenv_1.config)();
+const path_1 = __importDefault(require("path"));
+const fs_1 = __importDefault(require("fs"));
 const fastify_1 = __importDefault(require("fastify"));
 const cors_1 = __importDefault(require("@fastify/cors"));
+const secure_session_1 = __importDefault(require("@fastify/secure-session"));
+const passport_1 = __importDefault(require("@fastify/passport"));
+const auth_1 = __importDefault(require("@fastify/auth"));
+// @ts-ignore
+const passport_google_oauth2_1 = require("passport-google-oauth2");
 const index_1 = __importDefault(require("./routes/index"));
 const prisma_1 = __importDefault(require("./plugins/prisma"));
 const buildServer = () => {
@@ -20,16 +27,65 @@ const buildServer = () => {
             "http://localhost:5000",
             "http://127.0.0.1:3000",
             "http://127.0.0.1:5000",
-            "https://muras.vercel.app"
+            "https://muras.vercel.app",
+            "https://muras-test.netlify.app"
         ],
         credentials: true
     });
+    server.register(secure_session_1.default, {
+        key: fs_1.default.readFileSync(path_1.default.join(__dirname, "/..", "secret-key")),
+        logLevel: "debug",
+        cookie: process.env.NODE_ENV === "development"
+            ? {
+                path: "/"
+            }
+            : {
+                path: "/",
+                secure: true,
+                sameSite: "none",
+                domain: "muras.vercel.app"
+            }
+    });
+    server.register(passport_1.default.initialize());
+    server.register(passport_1.default.secureSession());
+    server.register(auth_1.default);
     server.register(prisma_1.default);
     server.get("/", (req, res) => {
         res.status(200).send({
-            message: "Hello World!"
+            message: "Hello World!",
+            //@ts-ignore
+            user: req.user?.displayName
         });
     });
+    server.get("/logout", (req, res) => {
+        req.logout();
+        res.status(200).send({
+            success: true
+        });
+    });
+    // ! Google Authenticator
+    passport_1.default.use(new passport_google_oauth2_1.Strategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.NODE_ENV === "development"
+            ? `${process.env.BACKEND_BASE_URL_DEV}/auth/google/callback`
+            : `${process.env.BACKEND_BASE_URL_PROD}/auth/google/callback`,
+        passReqToCallback: true
+    }, async function (request, accessToken, refreshToken, profile, done) {
+        console.log(profile);
+        done(null, profile);
+    }));
+    passport_1.default.registerUserSerializer(async (user, req) => {
+        return user;
+    });
+    passport_1.default.registerUserDeserializer(async (user, req) => {
+        return user;
+    });
+    server.get("/login", passport_1.default.authenticate("google", { scope: ["email", "profile"] }));
+    server.get("/auth/google/callback", passport_1.default.authenticate("google", {
+        successRedirect: "/",
+        failureRedirect: "/auth/google/failure"
+    }));
     server.register(index_1.default, {
         prefix: "/api/v1"
     });
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import { config } from "dotenv";
 config();
+import path from "path";
+import fs from "fs";
 import fastify, { FastifyInstance } from "fastify";
 import fastifyCors from "@fastify/cors";
+import fastifySecureSession from "@fastify/secure-session";
+import fastifyPassport from "@fastify/passport";
+import fastifyAuth from "@fastify/auth";
+// @ts-ignore
+import { Strategy as GoogleStrategy } from "passport-google-oauth2";
 import routes from "./routes/index";
 import prisma from "./plugins/prisma";
 import { PrismaClient } from "@prisma/client";
@@ -29,13 +36,88 @@ export const buildServer = () => {
 		credentials: true
 	});
 
+	server.register(fastifySecureSession, {
+		key: fs.readFileSync(path.join(__dirname, "/..", "secret-key")),
+		logLevel: "debug",
+
+		cookie:
+			process.env.NODE_ENV === "development"
+				? {
+						path: "/"
+				  }
+				: {
+						path: "/",
+						secure: true,
+						sameSite: "none",
+						domain: "muras.vercel.app"
+				  }
+	});
+
+	server.register(fastifyPassport.initialize());
+	server.register(fastifyPassport.secureSession());
+	server.register(fastifyAuth);
 	server.register(prisma);
 
 	server.get("/", (req, res) => {
 		res.status(200).send({
-			message: "Hello World!"
+			message: "Hello World!",
+			//@ts-ignore
+			user: req.user?.displayName
 		});
 	});
+
+	server.get("/logout", (req, res) => {
+		req.logout();
+		res.status(200).send({
+			success: true
+		});
+	});
+
+	// ! Google Authenticator
+	fastifyPassport.use(
+		new GoogleStrategy(
+			{
+				clientID: process.env.GOOGLE_CLIENT_ID,
+				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+				callbackURL:
+					process.env.NODE_ENV === "development"
+						? `${process.env.BACKEND_BASE_URL_DEV}/auth/google/callback`
+						: `${process.env.BACKEND_BASE_URL_PROD}/auth/google/callback`,
+				passReqToCallback: true
+			},
+			async function (
+				request: any,
+				accessToken: any,
+				refreshToken: any,
+				profile: any,
+				done: any
+			) {
+				console.log(profile);
+				done(null, profile);
+			}
+		)
+	);
+
+	fastifyPassport.registerUserSerializer(async (user, req) => {
+		return user;
+	});
+
+	fastifyPassport.registerUserDeserializer(async (user, req) => {
+		return user;
+	});
+
+	server.get(
+		"/login",
+		fastifyPassport.authenticate("google", { scope: ["email", "profile"] })
+	);
+
+	server.get(
+		"/auth/google/callback",
+		fastifyPassport.authenticate("google", {
+			successRedirect: "/",
+			failureRedirect: "/auth/google/failure"
+		})
+	);
 
 	server.register(routes, {
 		prefix: "/api/v1"
