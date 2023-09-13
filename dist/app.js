5c89a5488862145fcaa2830247b653ed2401f67f
Elcho911 <boss.armsport@gmail.com>
2023-09-14 02:29:36 +0600

    added google auth

--- dist/app.js
+++ dist/app.js
@@ -28,7 +28,8 @@ const buildServer = () => {
             "http://127.0.0.1:3000",
             "http://127.0.0.1:5000",
             "https://muras.vercel.app",
-            "https://muras-test.netlify.app"
+            "https://muras-test.netlify.app",
+            "https://muras-backend-f4e607bd17df.herokuapp.com"
         ],
         credentials: true
     });
