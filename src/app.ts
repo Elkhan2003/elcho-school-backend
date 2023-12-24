dd92671a10e8a4dada3bfe1976be193fdbfa185f
Elcho911 <boss.armsport@gmail.com>
2023-12-24 08:17:48 +0600

    update data

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,13 @@ export const buildServer = () => {
 				"http://127.0.0.1:3000",
 				"http://127.0.0.1:5000",
 				"http://localhost:5173",
+				"http://localhost:5174",
+				"http://localhost:5175",
+				"http://localhost:5176",
+				"http://localhost:5177",
+				"http://localhost:5178",
+				"http://localhost:5179",
+				"http://localhost:5180",
 				"https://muras-auth-test.vercel.app",
 				"https://muras-official.kg"
 			],
